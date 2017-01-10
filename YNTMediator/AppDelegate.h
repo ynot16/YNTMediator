97f1bafc6c723f018afa1eefbc5486e17a48125a
ynot16 <695110728@qq.com>
2017-01-10 10:46:10 +0800

    Initial Commit

---
+++ YNTMediator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YNTMediator
+//
+//  Created by bori－applepc on 2017/1/10.
+//  Copyright © 2017年 bori－applepc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YNTMediator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YNTMediator
+//
+//  Created by bori－applepc on 2017/1/10.
+//  Copyright © 2017年 bori－applepc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
